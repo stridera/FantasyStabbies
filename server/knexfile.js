19872673b22e1cb086e913f55d3cae234eee9189
Matthew Jones <matthew.jones@bluerivert.com>
2020-12-22 23:08:20 -0800

    Clean up

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,6 @@
 // Update with your config settings.
 require("dotenv").config({ path: "../.env" });
 
-console.log("Database URL: " + process.env.DATABASE_URL);
-console.log("Cert: " + process.env.DATABASE_CA_CERT);
-
 module.exports = {
   development: {
     client: "pg",
