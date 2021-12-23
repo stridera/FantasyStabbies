b7de1e7cb6319e75ec9daf70f1f9294b1b7ab6c4
Matthew Jones <stridera@gmail.com>
2021-12-23 02:24:51 -0800

    hardcode the state key for now.  session saving is not working

--- server/routes/auth/reddit.js
+++ server/routes/auth/reddit.js
@@ -3,16 +3,18 @@ const router = require("express").Router();
 const passport = require("passport");
 const crypto = require("crypto");
 
+const reddit_secret = crypto.createHash(process.env.SESSION_SECRET).toString("hex");
+
 router.route("/").get((req, res, next) => {
-  req.session.state = crypto.randomBytes(32).toString("hex");
+  // req.session.state = crypto.randomBytes(32).toString("hex");
   passport.authenticate("reddit", {
-    state: req.session.state,
+    state: reddit_secret,
   })(req, res, next);
 });
 
 router.route("/callback").get((req, res, next) => {
   // Check for origin via state token
-  if (req.query.state === req.session.state) {
+  if (req.query.state === reddit_secret) {
     passport.authenticate("reddit", {
       successRedirect: "/after_auth",
       failureRedirect: "/?error=auth_failed",
